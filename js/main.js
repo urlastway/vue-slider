75f79e5956682ac131795138a72f932cba300a89
urlastway <andriy.semenchenko13@gmail.com>
2022-05-21 15:39:20 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const app = new Vue({
+    el: `#root`,
+    data: {
+        currentIndex: 0,
+        images: [
+            {
+                src: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                src: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                src: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                src: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                src: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    },
+
+    methods: {
+        NextImg(){
+            if(this.currentIndex === 4){
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex++;
+            }
+        },
+        PrevImg(){
+            if(this.currentIndex === 0){
+                this.currentIndex = 4;
+            } else {
+                this.currentIndex--;
+            }
+        }
+    },
+});
+
+
